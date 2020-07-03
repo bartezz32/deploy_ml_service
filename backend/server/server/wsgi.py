a3c317a572bed3b3887aa21585f649d8a75ba9c5
Bartosz Majda <bartosz.majda32@gmail.com>
2020-07-03 16:12:40 +0200

    Extra Trees classifier added

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -19,7 +19,7 @@ application = get_wsgi_application()
 import inspect
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
-
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
 try:
     registry = MLRegistry()
     rf = RandomForestClassifier()
@@ -31,6 +31,18 @@ try:
                            owner="bartezz32",
                            algorithm_description="Random Forest with simple pre- and post-processing",
                            algorithm_code=inspect.getsource(RandomForestClassifier))
+
+    et = ExtraTreesClassifier()
+    registry.add_algorithm(endpoint_name="income_classifier",
+                           algorithm_object=et,
+                           algorithm_name="extra trees",
+                           algorithm_status="production",
+                           algorithm_version="0.0.1",
+                           owner="bartezz32",
+                           algorithm_description="Extra Trees with basic pre- and post-processing",
+                           algorithm_code=inspect.getsource(RandomForestClassifier)
+                           )
+
 
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
